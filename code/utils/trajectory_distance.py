9ca9a22b52ac09c0082aa5a44c227c2483a7f4e0
Bjørn Anders Flågen <baflagen@hotmail.com>
2023-02-01 17:02:55 +0100

    added functions for computing the decimal degree difference

--- code/utils/trajectory_distance.py
+++ code/utils/trajectory_distance.py
@@ -1,4 +1,5 @@
 from haversine import haversine, Unit
+import math 
 
 # Helper function to compute distance between a list of coordinates (Trajectory distance)
 # Haversine distance used
@@ -18,3 +19,35 @@ def calculate_trajectory_distance(positions: list[tuple[float]]) -> float:
 
         distance += haversine(from_location, to_location, unit=Unit.KILOMETERS)
     return distance
+
+
+def get_latitude_difference(distance: float) -> float:
+    """
+    Calculate the difference in latitude decimal degrees corresponding to a given distance
+    
+    Param   
+    ---
+    distance : float (km)
+
+    Returns
+    ---
+    latitude decimal degree difference as float
+    """
+    return distance / 110.574 
+
+
+def get_longitude_difference(distance: float, latitude: float) -> float:
+    """
+    Calculate the difference in longitude decimal degrees corresponding to a given distance
+    
+    Param   
+    ---
+    distance : float (km)
+        The distance for which the computation should be made
+    latitude : float
+        The latitude at which the computation is to be made
+    Returns
+    ---
+    longitide decimal degree difference as float
+    """
+    return distance/(111.320*math.cos(math.radians(latitude)))
